839441782d7e3a480554607f8e369e117bee7f30
Hannah Anderson <anderh3@mcmcaster.ca>
2021-06-21 15:21:06 -0400

    Fixed while loops. About to overhaul parser

--- src/main.cpp
+++ src/main.cpp
@@ -10,16 +10,19 @@
 // defining the analog pins for the sensors:
 // ETAPE measure water level, TURBID measure turbidity:
 # define ETAPE1 A0
-# define ETAPE2 A1
-# define ETAPE3 A2
-# define TURBID1 A3
-# define TURBID2 A4
+# define ETAPE2 A2
+# define ETAPE3 A4
+# define TURBID1 A1
+# define TURBID2 A3
 # define TURBID3 A5
+
+// value of the resistor on the eTape sensors:
+# define RESISTOR 560
 
 // specifying the number of samples to be averaged to smooth (average)
 // noisy sensor readings:
 # define NUMSAMPLES 5
-# define LEVELSAMPLES 3
+# define LEVELSAMPLES 5
 
 // handing the sample number to the sample variable:
 int samples_t[NUMSAMPLES];
@@ -77,12 +80,12 @@ void serialparse(int command) {
   float voltage_t;
   //I am not entirely sure yet that this function is properly retrieving these
   //variable values from the main loop()
-  float clearwater1;
-  float blackwater1;
-  float clearwater2;
-  float blackwater2;
-  float clearwater3;
-  float blackwater3;
+  //float clearwater1;
+  //float blackwater1;
+  //float clearwater2;
+  //float blackwater2;
+  //float clearwater3;
+  //float blackwater3;
 
   // all commands sent to a switch-case must be integers, no strings:
   switch(command) {
@@ -92,6 +95,7 @@ void serialparse(int command) {
     // read turbidity sensors:
     case 1:
     //// reading turbidity sensor 1:
+    Serial.println("check~NA~reading_turbidity");  // telling Pi case 1 has been activated:
     samples_t[NUMSAMPLES] = 0;
     analogRead(TURBID1);  // read to align multiplexer to A3:
     delay(10);
@@ -159,45 +163,61 @@ void serialparse(int command) {
     
     // short delay, then sending Pi the signal Arduino is ready for water changes:
     delay(100);
-    Serial.println("r~NA~NA");
+    Serial.println("r~NA~water_change_ready");
     break;
 
     // begining water change for tank chamber 1:
     case 2:
-    analogRead(ETAPE1);  // read to align multiplexer to A0:
+    Serial.println("check~1~water_change");  // status readout for Pi:
+    analogRead(ETAPE3);  // read to align multiplexer to A0:
     delay(10);
-    analogRead(ETAPE1);  // unused reading to deal with ADC lag:
+    analogRead(ETAPE3);  // unused reading to deal with ADC lag:
     delay(50);
     String waterdown = "yes";
     String clearup = "no";
     String blackup = "no";
+    String writeonce = "on";
     // draining the water until the appropriate water level is reached:
-    while (waterdown = "yes") {
+    while (waterdown == "yes") {
+      // exfill pump on:
       digitalWrite(exfill1, LOW);
       samples_w[LEVELSAMPLES] = 0;
+      // collecting multiple readings to stabilize noise:
       for (i=0; i< LEVELSAMPLES; i++) {
-        samples_w[i] = analogRead(ETAPE1);
+        samples_w[i] = analogRead(ETAPE3);
         delay(10);
       }
+      // averaging readings to stabilize noise:
       ave_level = 0;
       for (i=0; i< LEVELSAMPLES; i++) {
         ave_level += samples_w[i];
       }
       ave_level /= LEVELSAMPLES;
+      //exclusively for testing purposes, do not leave in code permanently
+      //Serial.print("check~1~");
+      //Serial.println(ave_level);
       // calibrated to drop the water to a height of ~18 cm:
-        // calibrated values: 120.44-120.76:
-      if (ave_level >= 120.50) {  
+      // must be calibrated for each sensor individually!
+        // calibrated values: 660.8-661.8:
+      if (ave_level >= 661) {  
         digitalWrite(exfill1, HIGH);
+        Serial.println("check~1~waterdown_reached");  // status readout for Pi:
+        // starting next stage of waterchange:
         waterdown = "no";
         clearup = "yes";
+        delay(50);
       }
     }
     // adding clearwater until the value sent by Pi is reached:
-    while (clearup = "yes") {
+    while (clearup == "yes") {
+      if (writeonce == "on") {
+        Serial.println("check~1~clearup_started");
+        writeonce = "off";
+      }
       digitalWrite(clearpump1, LOW);
       samples_w[LEVELSAMPLES] = 0;
       for (i=0; i< LEVELSAMPLES; i++) {
-        samples_w[i] = analogRead(ETAPE1);
+        samples_w[i] = analogRead(ETAPE3);
         delay(10);
       }
       ave_level = 0;
@@ -205,20 +225,29 @@ void serialparse(int command) {
         ave_level += samples_w[i];
       }
       ave_level /= LEVELSAMPLES;
-      if (ave_level >= clearwater1) {
+      //Serial.print("check~1~");
+      //Serial.println(ave_level);
+      if (ave_level <= clearwater1) {
+        Serial.println("check~1~clearup_reached");
         digitalWrite(clearpump1, HIGH);
         clearup = "no";
         blackup = "yes";
+        writeonce = "on";
+        delay(50);
       }
     }
     // adding blackwater until the maximum water amount is reached:
     // max water height is calibrated to ~25 cm:
-      // calibrated values: 95.08-95.74:
-    while (blackup = "yes") {
+      // calibrated values: 561.4-562:
+    while (blackup == "yes") {
+      if (writeonce == "on") {
+        Serial.println("check~1~blackup_started");
+        writeonce = "off";
+      }
       digitalWrite(blackpump1, LOW);
       samples_w[LEVELSAMPLES] = 0;
       for (i=0; i< LEVELSAMPLES; i++) {
-        samples_w[i] = analogRead(ETAPE1);
+        samples_w[i] = analogRead(ETAPE3);
         delay(10);
       }
       ave_level = 0;
@@ -226,10 +255,12 @@ void serialparse(int command) {
         ave_level += samples_w[i];
       }
       ave_level /= LEVELSAMPLES;
-      if (ave_level >= 95.10) {
+      if (ave_level <= 561.6) {
+        Serial.println("check~1~blackup_reached");
         digitalWrite(blackpump1, HIGH);
         blackup = "no";
         Serial.println("w~1~NA");
+        delay(50);
       }
     }
   break;
@@ -261,28 +292,47 @@ void loop() {
     String signpost = pi_out.substring(0, pi_out.indexOf("~"));
     // the "+1" ensures the "~" is not included in the message
     String message = pi_out.substring(pi_out.indexOf("~") + 1);
+    // tells Pi how Arduino divided the received message:
+    Serial.print("check~NA~");
+    Serial.print(signpost);
+    Serial.print(",");
+    Serial.println(message);
 
     /// parsing the Pi output based on the signpost:
     // signpost "a" prefaces commands to start cases in the switch-case:
     if (signpost.equals("a")) {
       // converting the Serial string to integer for use by the switch-case:
       int command = message.toInt();
+      Serial.println("check~NA~integer_conversion");
+      serialparse(command);
     }
     // signpost "1" indicates amount of clearwater to add to tank chamber 1:
     if (signpost.equals("1")) {
       float clearwater1 = message.toFloat();
+      Serial.print("check~1~chamber1_clear:");
+      Serial.println(clearwater1);
     }
     // signpost "2" indicates amount of clearwater to add to tank chamber 2:
     if (signpost.equals("2")) {
       float clearwater2 = message.toFloat();
+      Serial.print("check~2~chamber2_clear:");
+      Serial.println(clearwater2);
     }
     // signpost "3" indicates amoung of clearwater to add to tank chamber 3:
     if (signpost.equals("3")) {
       float clearwater3 = message.toFloat();
+      Serial.print("check~3~chamber3_clear:");
+      Serial.println(clearwater3);
     }
+
   }
 
-    // runs the response function coded above:
-    serialparse(command);
-
 }
+
+//notes: make sure you are calling on the right eTape when testing
+//remember: change back the Case 2 eTape to ETAPE1 for final version!!!
+
+//For some reason the clearup for loop is not properly reading the 
+
+//once this is piloted add a failsafe where if a relay is on for too long (pilot water change time)
+//then it automatically turns off (and sends me a message?)
